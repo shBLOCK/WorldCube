12bbfd09e8d4b06c86a8475be102899b2af703f2
shBLOCK <shblock3@gmail.com>
2026-03-04 20:46:44 -0800

    tauri-media-pipe-face progress

---
+++ software/tauri-media-pipe-face/src/main.ts
@@ -0,0 +1,100 @@
+// import {invoke} from "@tauri-apps/api/core";
+import {DrawingUtils, FaceLandmarker, FaceLandmarkerResult, FilesetResolver} from "@mediapipe/tasks-vision";
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+async function loadFaceLandmarker() {
+    return await FaceLandmarker.createFromOptions(
+        await FilesetResolver.forVisionTasks("node_modules/@mediapipe/tasks-vision/wasm"),
+        {
+            baseOptions: {
+                modelAssetPath: "src/assets/face_landmarker.task",
+                delegate: "GPU"
+            },
+            runningMode: "VIDEO",
+            outputFacialTransformationMatrixes: true,
+            numFaces: 1
+        }
+    );
+}
+
+let faceLandmarker = await loadFaceLandmarker();
+
+const camera = document.getElementById("camera") as HTMLVideoElement;
+
+navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
+    camera.srcObject = stream;
+    camera.addEventListener("loadeddata", handleCameraFrame)
+});
+
+const drawingUtils = new DrawingUtils(canvas.getContext("2d") as CanvasRenderingContext2D);
+
+let lastCameraTime = -1;
+let faceLandmarkerResult: FaceLandmarkerResult | null = null;
+
+async function handleCameraFrame() {
+    const visualWidth = 720;
+    const visualHeight = visualWidth * (camera.videoHeight / camera.videoWidth);
+    camera.style.width = canvas.style.width = `${visualWidth}px`;
+    camera.style.height = canvas.style.height = `${visualHeight}px`;
+    canvas.width = visualWidth;
+    canvas.height = visualHeight
+
+    const startMs = performance.now();
+    if (lastCameraTime !== camera.currentTime) {
+        lastCameraTime = camera.currentTime;
+        faceLandmarkerResult = faceLandmarker.detectForVideo(camera, startMs);
+    }
+
+    if (faceLandmarkerResult !== null && faceLandmarkerResult.faceLandmarks) {
+        for (const landmarks of faceLandmarkerResult.faceLandmarks) {
+            // drawingUtils.drawConnectors(
+            //     landmarks,
+            //     FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+            //     {color: "#C0C0C070", lineWidth: 1}
+            // );
+            drawingUtils.drawConnectors(
+                landmarks,
+                FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+                {color: "#FF3030"}
+            );
+            // drawingUtils.drawConnectors(
+            //     landmarks,
+            //     FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+            //     {color: "#FF3030"}
+            // );
+            drawingUtils.drawConnectors(
+                landmarks,
+                FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+                {color: "#30FF30"}
+            );
+            // drawingUtils.drawConnectors(
+            //     landmarks,
+            //     FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+            //     {color: "#30FF30"}
+            // );
+            // drawingUtils.drawConnectors(
+            //     landmarks,
+            //     FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+            //     {color: "#E0E0E0"}
+            // );
+            // drawingUtils.drawConnectors(
+            //     landmarks,
+            //     FaceLandmarker.FACE_LANDMARKS_LIPS,
+            //     {color: "#E0E0E0"}
+            // );
+            // drawingUtils.drawConnectors(
+            //     landmarks,
+            //     FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+            //     {color: "#FF3030"}
+            // );
+            // drawingUtils.drawConnectors(
+            //     landmarks,
+            //     FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+            //     {color: "#30FF30"}
+            // );
+        }
+    }
+
+    window.requestAnimationFrame(handleCameraFrame);
+}
